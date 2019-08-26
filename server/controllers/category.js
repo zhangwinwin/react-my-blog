710e7bc08e5a30a18ae750ddd0d2300f4a53694e
zhangjinhui <344879340@qq.com>
2019-08-26 20:32:26 +0800

    feat: 服务端增加通过目录获取文章接口

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -10,3 +10,29 @@ export async function getCategories (ctx) {
     data
   }
 }
+
+export async function getArticlesByCate (ctx) {
+  let { page = 1, pageSize = 10, name } = ctx.query
+  let offset = (page - 1) * pageSize
+
+  pageSize = parseInt(pageSize)
+
+  const data = await db.article.findAndCountAll({
+    attributes: ['id', 'title', 'createdAt'],
+    include: [{
+      model: db.category,
+      attributes: ['name'],
+      where: {
+        name
+      }
+    }],
+    offset,
+    limit: pageSize,
+    order: [['createdAt', 'DESC']],
+    distinct: true
+  })
+  ctx.body = {
+    code: 200,
+    data
+  }
+}
--- server/router/category.js
+++ server/router/category.js
@@ -1,8 +1,9 @@
 import Router from 'koa-router'
-import { getCategories } from '../controllers/category'
+import { getCategories, getArticlesByCate } from '../controllers/category'
 
 const router = new Router()
 
 router.get('/', getCategories)
+router.get('/article', getArticlesByCate)
 
 export default router
