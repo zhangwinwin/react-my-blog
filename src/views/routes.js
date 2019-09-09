f7c140dd22902e5af1de295a6c5bb9ff1d512827
zhangjinhui <344879340@qq.com>
2019-09-09 15:20:13 +0800

    feat: 前端：编写list页面

--- src/views/routes.js
+++ src/views/routes.js
@@ -2,6 +2,8 @@ import Layout from '@/component/layout/layout.jsx'
 // import asyncComponent from '@/utils/asyncComponent'
 import Home from './home/home.jsx'
 import Article from './article/article.jsx'
+import Categories from './categories/categories.jsx'
+import List from './list/list.jsx'
 // const Article = asyncComponent(() => import('./article/article.jsx'))
 
 export default {
@@ -10,6 +12,9 @@ export default {
   component: Layout,
   childRoutes: [
     { path: '', component: Home},
-    { path: 'article/:id', component: Article }
+    { path: 'article/:id', component: Article },
+    { path: 'categories', component: Categories },
+    { path: 'categories/:name', component: List },
+    { path: 'tags/:name', component: List }
   ]
 }
