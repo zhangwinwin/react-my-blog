5efb4c9912388504df82e408e288ac54323100ca
zhangwinwin <344879340@qq.com>
2019-12-24 16:24:04 +0800

    修改about页面

--- src/views/about/about.jsx
+++ src/views/about/about.jsx
@@ -20,18 +20,18 @@ function About(props) {
       })
     }
     fetchList()
-  }, [])
+  }, [props])
   
 
   return (
     <div className="content-inner-wrapper about">
       <AuthorAvatar />
-      <span className="desc">开发工程师</span>
+      <span className="desc">前端开发工程师</span>
       <Divider orientation="left">博客简述</Divider>
       {/* <p>主要是用来记录博主学习而作！</p> */}
       <p>本博客使用的技术为 react  + antd + koa2 + mysql</p>
       <p>
-        源码地址为{' '}
+        源码地址为
         <a target="_blank" rel="noreferrer noopener" href="https://github.com/zhangwinwin/react-my-blog">
           github
         </a>
@@ -41,16 +41,12 @@ function About(props) {
       <ul className="about-list">
         <li>姓名：张</li>
         <li>学历：本科</li>
-        {/* <li>
-          联系方式：
-          <Icon type="qq" /> 
-          <Divider type="vertical" />
-          <i className="iconfont icon-email" />
-          <a href="#">#</a>
-        </li> */}
         <li>坐标：广州市</li>
         <li>
-          其他博客地址：
+          其他论坛：
+          <a target="_blank" rel="noreferrer noopener" href="https://juejin.im/user/5c6b66e9e51d4539a642640c">
+            掘金
+          </a>
         </li>
         <li>
           技能
@@ -60,19 +56,15 @@ function About(props) {
               <Rate defaultValue={4} disabled />
             </li>
             <li>
-              react vue 框架：熟练掌握使用！
+              react vue 框架：掌握使用！
               <Rate defaultValue={4} disabled />
             </li>
             <li>
-              es6：日常开发必备，以及掌握基本面向对象编程实现！
-              <Rate defaultValue={4} disabled />
-            </li>
-            <li>
-              webpack: 入门级别，可以对脚手架进行针对性的配置！
+              webpack: 能针对项目做优化
               <Rate defaultValue={3} disabled />
             </li>
             <li>
-              node mysql：针对需求可以做到简单的数据库设计、接口的开发与设计！
+              nodeJs mysql：针对需求可以做到简单的数据库设计、接口的开发与设计！
               <Rate defaultValue={3} disabled />
             </li>
           </ul>
