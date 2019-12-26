141a1cb6dafbf61e81377ea909d40beb69edc095
zhangwinwin <344879340@qq.com>
2019-12-26 16:56:10 +0800

    增加写文章按钮

--- src/component/header/userInfo.jsx
+++ src/component/header/userInfo.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { register, logout } from '@/store/user/user-action.js'
 import { openAuthModal } from '@/store/common/common-actions.js'
 import AuthModal from '@/component/authModal/authModal.jsx'
+import { Link } from 'react-router-dom'
 import { Button, Dropdown, Avatar, Menu } from 'antd'
 
 
@@ -28,6 +29,11 @@ class UserInfo extends Component {
           <span className="user-logout" onClick={this.props.logout}>
             退出登录
           </span>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to={`/write`}>
+            写文章
+          </Link>
         </Menu.Item>
       </Menu>
     )
