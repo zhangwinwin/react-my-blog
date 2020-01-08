a639eb5ef87800d3a206528a7a748e1148e465cd
zhangwinwin <344879340@qq.com>
2020-01-08 16:35:03 +0800

    修改配置文件

--- src/env.js
+++ src/env.js
@@ -5,7 +5,7 @@ let baseURL
 if(process.env.NODE_ENV === 'development'){
   baseURL = 'http://localhost:3030/api'
 }else{
-  baseURL = 'http://localhost:3030/api'
+  baseURL = 'http://106.53.71.181:3030/api'
 }
 
 
