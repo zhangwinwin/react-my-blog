1eed773c96f1136d6bedcbef8d52cecb68e61baa
zhangwinwin <344879340@qq.com>
2020-03-29 21:42:47 +0800

    增加备案信息

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './footer.less'
+
+const footer = () => {
+  return (
+    <div className='footer__beian'>
+      <img className='footer__icon' src="https://guanertong.cn/zb_users/upload/2019/07/20190709184612156266917296822.png" alt="备案标识"></img>
+      <a href='http://beian.miit.gov.cn/' className="footer__link">粤ICP备20011065号</a>
+    </div>
+  )
+}
+
+export default footer
